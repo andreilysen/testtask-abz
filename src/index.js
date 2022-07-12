dda038eebe475313b4e6bbbe5f4dd52b90e82652
Andrei Lysenkovskiy <andreilysen@ukr.net>
2022-07-12 18:12:49 +0300

    start header

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/scss/index.scss";
-import App from "./App";
+import App from "./Components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
